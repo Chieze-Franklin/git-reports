995ca1cebf79c6ecb7f20e4c00ca1049c28c9d96
Franklin Chieze <chieze.franklin@gmail.com>
2017-12-10 22:46:36 +0100

    New design for NotFound component

--- src/js/pages/NotFound.js
+++ src/js/pages/NotFound.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-class NotFound extends React.Component {
-    render() {
-        return (
-            <div className="container-fluid">Oops. Seems like we can't find what you are looking for!
-            </div>
-        )
-    }
-}
+const NotFound = () => (
+    <div className="container-fluid">
+        <div className="container center">
+        <h1>404 NOT FOUND</h1>
+        <p>Oops. Seems like we can't find what you are looking for!</p>
+        <Link to="/">HOME</Link>
+        </div>
+    </div>
+);
 
 export default NotFound;
